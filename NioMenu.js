ee0db954974137055f9bbec280686c59e12761d3
blackdartq <brody.prestwich666@gmail.com>
2017-01-12 14:51:57 -0700

    first commit

---
+++ NioMenu.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        NioMenu
+// @namespace   BrodyPrestwich
+// @description a menu system to navigate nio api.
+// @include     http://192.168.1.1*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
